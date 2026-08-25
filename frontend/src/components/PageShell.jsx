6427654927b0692389e3021940ce879883ed29d4
Sawanya Kodagoda <sawanya@gmail.com>
2026-08-25 19:56:47 +0530

    Revert "Feature/pickup request"

--- frontend/src/components/PageShell.jsx
+++
@@ -1,8 +0,0 @@
-export default function PageShell({ title, description }) {
-  return (
-    <div className="page-shell">
-      <h1>{title}</h1>
-      {description && <p className="page-shell-desc">{description}</p>}
-    </div>
-  )
-}
